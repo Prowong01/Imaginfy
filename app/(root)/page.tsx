480af0f50b36b401dabaa87dd43e4e892d17ea6c
cpwong <cpwong@topglove.com.my>
2024-02-19 11:17:54 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default HomePage;

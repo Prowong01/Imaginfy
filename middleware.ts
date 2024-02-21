5ae4f4b20cb12ae7937b004fa3e5fefc5ac9a282
cpwong <cpwong@topglove.com.my>
2024-02-21 17:16:24 +0800

    modify middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk'],
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: ['/api/webhooks/clerk(.*)']
 });
  
 export const config = {
